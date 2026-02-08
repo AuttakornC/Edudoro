a9d98c5e6b3f5c63a94aa869c626050784012f42
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2026-02-08 19:52:14 +0700

    feat: all pages route

---
+++ lib/pages/avatar_change.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AvatarChangePage extends StatelessWidget {
+  const AvatarChangePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/route.dart
@@ -0,0 +1,34 @@
+import 'package:edudoro/pages/avatar_change.dart';
+import 'package:edudoro/pages/friend.dart';
+import 'package:edudoro/pages/goal.dart';
+import 'package:edudoro/pages/home.dart';
+import 'package:edudoro/pages/landing.dart';
+import 'package:edudoro/pages/profile.dart';
+import 'package:edudoro/pages/setting.dart';
+import 'package:edudoro/pages/shop.dart';
+import 'package:edudoro/pages/sign_in.dart';
+import 'package:edudoro/theme.dart';
+import 'package:flutter/material.dart';
+
+class RouteApp extends StatelessWidget {
+  const RouteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Edudoro',
+      theme: theme,
+      routes: {
+        "/": (context) => const LandingPage(),
+        "/sign_in": (context) => const SignInPage(),
+        "/avatar_change": (context) => const AvatarChangePage(),
+        "/friend": (context) => const FriendPage(),
+        "/goal": (context) => const GoalPage(),
+        "/home": (context) => const HomePage(),
+        "/profile": (context) => const ProfilePage(),
+        "/setting": (context) => const SettingPage(),
+        "/shop": (context) => const ShopPage(),
+      },
+    );
+  }
+}
