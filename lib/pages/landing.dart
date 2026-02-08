8475b833ff2a3bbd2fb2fde5f97e2a630ad624da
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2026-02-08 20:20:03 +0700

    feat: create Button for navigate to each pages

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -5,6 +5,47 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: TempNavigator(),
+        ),
+      ),
+    );
+  }
+}
+
+class TempNavigator extends StatelessWidget {
+  const TempNavigator({super.key});
+
+  static final List<String> _pageList = [
+    "/avatar_change",
+    "/friend",
+    "/goal",
+    "/home",
+    "/profile",
+    "/setting",
+    "/shop",
+    "/sign_in",
+    "/sign_up",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: _pageList.map((path) => generateButton(context, path)).toList(),
+    );
+  }
+
+  Widget generateButton(BuildContext context, String path) {
+    String label = "go to $path";
+
+    return Center(
+      child: TextButton(
+        onPressed: () => {Navigator.of(context).pushNamed(path)},
+        child: Text(label),
+      ),
+    );
   }
 }
