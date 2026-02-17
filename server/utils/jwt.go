dc2eed44213392506a761ddcd7893c7e740a7d05
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2026-02-17 12:54:30 +0700

    feat(server): jwt utils

---
+++ server/utils/jwt.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func JWTCreateToken(accountId string) (string, error) {
+	claims := jwt.MapClaims{
+		"authorized": true,
+		"account_id": accountId,
+		"created_at": time.Now().UnixMilli(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func ValidateToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+}
