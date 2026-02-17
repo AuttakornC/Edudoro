3a0aabe990cef4a6bb7892a33c8b0c5d380d3dde
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2026-02-17 12:54:26 +0700

    feat(server): env usage

--- server/main.go
+++ server/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/AuttakornC/Edudoro/server/models"
 	"github.com/AuttakornC/Edudoro/server/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Can't load env:", err)
+	}
+
 	models.ConnectDatabase()
 
 	r := routes.SetupRouter()
-	r.Run(":3000")
+	r.Run(":" + os.Getenv("PORT"))
 }
