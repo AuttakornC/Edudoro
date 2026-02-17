242bdc800fd33f2d2e7b74eba07b12c44a133faf
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2026-02-17 12:55:18 +0700

    fix(server): remove test route

--- server/routes/auth_route.go
+++ server/routes/auth_route.go
@@ -9,7 +9,6 @@ func authRouteSetup(r *gin.RouterGroup) {
 	authRoute := r.Group("/auth")
 	{
 		authRoute.POST("/sign-in", controllers.AuthSignIn)
-		authRoute.GET("/sign-in", func(ctx *gin.Context) { ctx.String(200, "Hello, Sign In.") })
 		authRoute.POST("/sign-up", controllers.AuthSignUp)
 	}
 }
