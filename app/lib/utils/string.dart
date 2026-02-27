175247ba5cb3c6b1a93dce82f9b94af549483560
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2026-02-27 14:03:33 +0700

    feat(util): secondToMinuteFormat func

--- app/lib/utils/string.dart
+++ app/lib/utils/string.dart
@@ -1,3 +1,7 @@
 String padZero(int value, int width) {
   return value.toString().padLeft(width, '0');
 }
+
+String secondToMinuteFormat(int second) {
+  return "${(second / 60).floor()}:${padZero((second % 60).toInt(), 2)}";
+}
